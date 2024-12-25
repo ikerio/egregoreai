965540c6c063d2697d8a357318f3fa87e097985d
Egregore <egregore@Egregores-Mac-mini.local>
2024-12-25 11:01:15 -0800

    Update gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-export default {
-  base: '/', // Update this when using custom domain
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // or '/egregore-website/' if not using custom domain
   build: {
     outDir: 'dist',
   }
-}
+})
